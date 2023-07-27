c309544704309f2f9fd646b91cc24bac18a0ef01
Emily <efg12103@gmail.com>
2023-07-27 14:30:19 -0400

    add getters, setters, and constructor to ToiletPaperPackage

--- app/src/main/java/com/tolietpaperprices/Model/TolietPaperPackage.java
+++ app/src/main/java/com/tolietpaperprices/Model/TolietPaperPackage.java
@@ -1,5 +1,6 @@
 package com.tolietpaperprices.Model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -7,17 +8,117 @@ import java.time.LocalDateTime;
  *
  * @author Emily
  */
-public class TolietPaperPackage {
+public class TolietPaperPackage implements Serializable {
+
+    public enum Style{
+        SOFT, STRONG, NORMAL
+    }
 
     String brand;
-    String style; //soft, heavy duty, normal, etc
+    Style style;
     double price;
     int numRolls;
     int numSquaresPerRoll;
-    int pricePerSquare;
+    double pricePerSquare;
     String storeName;
     String storeTown;
     LocalDateTime dateOfEntry;
 
+    /**
+     * Default constructor
+     * @param brand
+     * @param style
+     * @param price
+     * @param numRolls
+     * @param numSquaresPerRoll
+     * @param storeName
+     * @param storeTown
+     */
+    public TolietPaperPackage(String brand, Style style, double price, int numRolls,
+                              int numSquaresPerRoll, String storeName, String storeTown) {
+        this.brand = brand;
+        this.style = style;
+        this.price = price;
+        this.numRolls = numRolls;
+        this.numSquaresPerRoll = numSquaresPerRoll;
+        this.storeName = storeName;
+        this.storeTown = storeTown;
+        this.dateOfEntry = LocalDateTime.now();
+        this.pricePerSquare = this.price / (this.numSquaresPerRoll * this.numRolls);
+    }
 
+    /**
+     * Getters and setters for TolietPaperPackage
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumRolls() {
+        return numRolls;
+    }
+
+    public void setNumRolls(int numRolls) {
+        this.numRolls = numRolls;
+    }
+
+    public int getNumSquaresPerRoll() {
+        return numSquaresPerRoll;
+    }
+
+    public void setNumSquaresPerRoll(int numSquaresPerRoll) {
+        this.numSquaresPerRoll = numSquaresPerRoll;
+    }
+
+    public double getPricePerSquare() {
+        return pricePerSquare;
+    }
+
+    public void calculatePricePerSquare() {
+        this.pricePerSquare = this.price / (this.numSquaresPerRoll * this.numRolls);
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreTown() {
+        return storeTown;
+    }
+
+    public void setStoreTown(String storeTown) {
+        this.storeTown = storeTown;
+    }
+
+    public LocalDateTime getDateOfEntry() {
+        return dateOfEntry;
+    }
+
+    public void setDateOfEntry(LocalDateTime dateOfEntry) {
+        this.dateOfEntry = LocalDateTime.now();
+    }
 }
