2aa951127a2c1fcbef59d2edde3852a68b67f8ef
Emily <efg12103@gmail.com>
2023-07-27 13:55:00 -0400

    git init

---
+++ app/src/main/java/com/tolietpaperprices/Controller/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tolietpaperprices.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tolietpaperprices.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
