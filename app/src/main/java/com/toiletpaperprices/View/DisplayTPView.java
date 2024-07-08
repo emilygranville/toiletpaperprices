77481c590a40369347c16a7770a2d449c2e04959
Emily <32372751+emilygranville@users.noreply.github.com>
2024-07-08 18:04:43 -0400

    clean up

--- app/src/main/java/com/toiletpaperprices/View/DisplayTPView.java
+++ app/src/main/java/com/toiletpaperprices/View/DisplayTPView.java
@@ -10,10 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.toiletpaperprices.Controller.MainActivity;
